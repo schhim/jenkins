cbc859721425dbd3b4569b0370500ed58043d23e
schhim <60415709+schhim@users.noreply.github.com>
2021-12-08 11:05:37 -0600

    updated security groups and key pairs

--- Instance Setup/instance.sh
+++ Instance Setup/instance.sh
@@ -1,13 +1,13 @@
 aws ec2 run-instances \
 --image-id ami-0629230e074c580f2 \
---security-group-ids sg-02832c58304b8c2fd \
+--security-group-ids sg-4e02e85f \
 --instance-type t2.medium \
---key-name OhioKeyPair \
+--key-name skillbridge \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Jenkins}]'
 
 aws ec2 run-instances \
 --image-id ami-0629230e074c580f2 \
---security-group-ids sg-02832c58304b8c2fd \
+--security-group-ids sg-4e02e85f \
 --instance-type t2.medium \
---key-name OhioKeyPair \
+--key-name skillbridge \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=WebServer}]'
